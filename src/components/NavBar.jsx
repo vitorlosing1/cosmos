135179df5f575404061f820d76d2c91739433ad7
vitorlosing1 <vitorluricii@hotmail.com>
2024-03-16 20:09:29 -0300

    navbar-update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./NavBar.css";
 import { LogoIcon } from "../assets/svg/LogoIcon";
 import { SearchIcon } from "../assets/svg/SearchIcon";
+import ReloadPage from "./ReloadPage";
 
 function NavBar() {
   return (
     <header>
       <div className="left-bar">
-        <div className="logo">
+        <div className="logo" onClick={ReloadPage}>
           <LogoIcon />
           COSMOS
         </div>
+      </div>
+      <div className="mid-bar">
         <span>PAGE</span>
         <span>PAGE</span>
         <span>PAGE</span>
---
+++ src/components/ReloadPage.jsx
@@ -0,0 +1,7 @@
+const ReloadPage = () => {
+  return window.location.reload();
+};
+export default ReloadPage;
+
+//funcao para recarregar a página
+//ex: usuário clica na logo
