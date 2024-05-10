be62538d226aa8aa492217bdea8648b5c3bdfd73
vitorlosing1 <vitorluricii@hotmail.com>
2024-05-09 21:33:17 -0300

    mobile-menu-update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Header.scss";
 import { LogoIcon } from "../assets/svg/LogoIcon";
-import { SearchIcon } from "../assets/svg/SearchIcon";
+import { MenuIcon } from "../assets/svg/MenuIcon";
 import ReloadPage from "./ReloadPage";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <header>
       <div className="logo-container">
@@ -29,7 +35,25 @@ function NavBar() {
           </Link>
         </nav>
 
-        <nav className="menu-mobile"></nav>
+        <button className="menu-mobile" onClick={toggleModal}>
+          <MenuIcon />
+          {isModalOpen && (
+            <nav className="mobile-container">
+              <Link to={"/"} onClick={ReloadPage}>
+                <span>HOME</span>
+              </Link>
+              <Link to={"/foto-do-dia"}>
+                <span>FOTO DO DIA</span>
+              </Link>
+              <Link to={"/noticias"}>
+                <span>NOTÍCIAS</span>
+              </Link>
+              <Link to={"/marte"}>
+                <span>MARTE</span>
+              </Link>
+            </nav>
+          )}
+        </button>
       </div>
     </header>
   );
