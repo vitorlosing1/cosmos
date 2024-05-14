c69437262711ce774b26ae834731fd04e82fe513
vitorlosing1 <vitorluricii@hotmail.com>
2024-05-14 01:44:03 -0300

    news update

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -7,6 +7,7 @@ function News() {
   const [news, setNews] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
+  const [hasMoreNews, setHasMoreNews] = useState(true); // Estado para controlar se há mais notícias para carregar
   const newsPerPage = 20;
 
   useEffect(() => {
@@ -22,6 +23,11 @@ function News() {
           setNews((prevNews) => [...prevNews, ...newNews]);
         }
         setLoading(false); // Atualizar o estado para indicar que o carregamento terminou
+
+        // Verificar se há mais notícias disponíveis
+        if (fetchedNews.length <= endIndex) {
+          setHasMoreNews(false);
+        }
       } catch (error) {
         console.error("Erro ao buscar notícias:", error);
       }
@@ -30,21 +36,7 @@ function News() {
     fetchNews();
   }, [currentPage]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop !==
-        document.documentElement.offsetHeight
-      )
-        return;
-      nextPage();
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const nextPage = () => {
+  const loadMoreNews = () => {
     setCurrentPage((prevPage) => prevPage + 1);
   };
 
@@ -59,26 +51,33 @@ function News() {
           <DarkIcon />
         </div>
       ) : (
-        news.map((item, index) => (
-          <div className="news-box" key={index}>
-            <div className="news-item">
-              <img className="news-img" src={item.image} alt={item.title} />
-              <div className="news-content">
-                <small className="date">{item.publishedAt}</small>
-                <h4 className="news-title">{item.title}</h4>
-                <p>{item.content}</p>
-                <small className="source">Fonte: {item.sourceName}</small>
-                <button
-                  className="read-article"
-                  onClick={() => openArticle(item.link)}
-                >
-                  Ler artigo completo em {item.sourceName}
-                </button>
+        <>
+          {news.map((item, index) => (
+            <div className="news-box" key={index}>
+              <div className="news-item">
+                <img className="news-img" src={item.image} alt={item.title} />
+                <div className="news-content">
+                  <small className="date">{item.publishedAt}</small>
+                  <h4 className="news-title">{item.title}</h4>
+                  <p>{item.content}</p>
+                  <small className="source">Fonte: {item.sourceName}</small>
+                  <button
+                    className="read-article-btn"
+                    onClick={() => openArticle(item.link)}
+                  >
+                    Ler artigo completo em {item.sourceName}
+                  </button>
+                </div>
               </div>
+              <hr />
             </div>
-            <hr />
-          </div>
-        ))
+          ))}
+          {hasMoreNews && (
+            <button className="load-more-btn" onClick={loadMoreNews}>
+              Carregar mais notícias
+            </button>
+          )}
+        </>
       )}
     </main>
   );
